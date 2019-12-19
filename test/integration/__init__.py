bfebf35ed2260e2d65b0d6d3fb0b84eb0b0b86a3
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-12-19 13:43:21 -0800

    change: remove unused test files

--- test/integration/__init__.py
+++ test/integration/__init__.py
@@ -17,12 +17,8 @@ import os
 resources_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'resources'))
 mnist_path = os.path.join(resources_path, 'mnist')
 mnist_script = os.path.join(mnist_path, 'mnist.py')
-fastai_path = os.path.join(resources_path, 'fastai')
-fastai_cifar_script = os.path.join(fastai_path, 'train_cifar.py')
-fastai_mnist_script = os.path.join(fastai_path, 'mnist.py')
 data_dir = os.path.join(mnist_path, 'data')
 training_dir = os.path.join(data_dir, 'training')
-dist_operations_path = os.path.join(resources_path, 'distributed_operations.py')
 
 model_cpu_dir = os.path.join(mnist_path, 'model_cpu')
 model_cpu_1d_dir = os.path.join(model_cpu_dir, '1d')
