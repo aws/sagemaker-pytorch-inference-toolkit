705c3a1ef5e07e2f8519baf1414554f53ec70a24
Sai Parthasarathy Miduthuri <saimidu@amazon.com>
2020-01-09 00:13:50 -0800

    revert remote-test changes

--- docker/build_artifacts/mms-entrypoint.py
+++ docker/build_artifacts/mms-entrypoint.py
@@ -17,12 +17,11 @@ import subprocess
 import sys
 import os.path
 
-
 if not os.path.exists("/opt/ml/input/config"):
     subprocess.call(['python', '/usr/local/bin/deep_learning_container.py', '&>/dev/null', '&'])
 
 if sys.argv[1] == 'serve':
-	from sagemaker_pytorch_serving_container import serving
+    from sagemaker_pytorch_serving_container import serving
     serving.main()
 else:
     subprocess.check_call(shlex.split(' '.join(sys.argv[1:])))
