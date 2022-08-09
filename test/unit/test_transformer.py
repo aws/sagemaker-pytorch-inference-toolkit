7fad13e07ba97b8de49b7ad1847d528fe33f774e
Wei Chu <weichu@amazon.com>
2022-08-09 14:16:51 -0700

    fix sanity

--- test/unit/test_transformer.py
+++ test/unit/test_transformer.py
@@ -9,11 +9,11 @@
 # or in the 'license' file accompanying this file. This file is
 # distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License. 
+# language governing permissions and limitations under the License.
 
 from __future__ import absolute_import
 
-from mock import Mock, patch 
+from mock import Mock, patch
 import pytest
 
 from sagemaker_inference import environment
