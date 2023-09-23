9a0758eed88e70c97132bb1a1ee975896c1d0e9e
Ubuntu <ubuntu@ip-172-31-4-70.us-west-2.compute.internal>
2023-09-23 03:42:08 +0000

    Run GPU local integration tests

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 
     # We don't declare our dependency on torch here because we build with
