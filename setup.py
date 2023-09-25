8b0a3eafc619d181a0ae562b1ee04b6e01955269
Ubuntu <ubuntu@ip-172-31-4-70.us-west-2.compute.internal>
2023-09-25 22:22:34 +0000

    Rerun unit and integration tests with py38

--- setup.py
+++ setup.py
@@ -47,8 +47,8 @@ setup(
         "Programming Language :: Python",
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-    #    'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 
     # We don't declare our dependency on torch here because we build with
